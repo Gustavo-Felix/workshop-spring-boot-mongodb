cd1b4df57111df8532a0e2dd838b918f77edd85a
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-11-04 05:33:02 -0300

    feat: implement findById endpoint and standardize exception handling

--- src/main/java/com/gustavofelix/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/gustavofelix/workshopmongodb/resources/UserResource.java
@@ -7,11 +7,10 @@ import com.gustavofelix.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,13 @@ public class UserResource {
                 .map(UserDTO::new)
                 .toList();
         return ResponseEntity.ok().body(userDTOList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.ok().body(userDTO);
     }
 
 
