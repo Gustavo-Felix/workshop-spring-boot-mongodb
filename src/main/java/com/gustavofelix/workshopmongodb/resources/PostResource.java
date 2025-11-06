70507ec31f13dd365c7cdde049d0c112092462c3
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-11-06 08:07:09 -0300

    feat: add endpoint to return post by ID

--- src/main/java/com/gustavofelix/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/gustavofelix/workshopmongodb/resources/PostResource.java
@@ -5,6 +5,7 @@ import com.gustavofelix.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class PostResource {
         List<Post> posts = postService.findAll();
 
         return ResponseEntity.ok().body(posts);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
     }
 
 }
--- src/main/java/com/gustavofelix/workshopmongodb/services/PostService.java
+++ src/main/java/com/gustavofelix/workshopmongodb/services/PostService.java
@@ -2,10 +2,12 @@ package com.gustavofelix.workshopmongodb.services;
 
 import com.gustavofelix.workshopmongodb.domain.Post;
 import com.gustavofelix.workshopmongodb.repository.PostRepository;
+import com.gustavofelix.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -15,6 +17,11 @@ public class PostService {
 
     public List<Post> findAll() {
         return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException(id));
     }
 
 }
